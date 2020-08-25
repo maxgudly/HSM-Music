dc10314938a76e23345c89af4828c6547d9e4e7d
Maxwell Aruakpor <aruakpo@uwindsor.ca>
2020-08-25 19:41:41 -0400

    Added  Database

--- HSM_Music/Models/Customers.cs
+++ HSM_Music/Models/Customers.cs
@@ -9,6 +9,9 @@ namespace HSM_Music.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-       
+
+        public bool isSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
